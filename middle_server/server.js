f809b1a1d7b27632546ff8172cb30228f0ad9463
ysyss <alsdbtjd0103@naver.com>
2023-01-11 17:19:28 +0900

    cheerio -> selenium으로 수정 예정

--- middle_server/server.js
+++ middle_server/server.js
@@ -1,19 +1,55 @@
-const express = require('express');
-const path = require('path');
-const app =express();
-
-const PORT=4000;
+const express = require("express");
+const path = require("path");
+const app = express();
+const cheerio = require("cheerio");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const PORT = 4000;
 app.use(express.json());
-var cors = require('cors');
-app.use(cors());
+
+var cors = require("cors");
+
+let corsOptions = {
+  origin: "*", // 출처 허용 옵션
+  credential: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, "../middle/build")));
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.listen(PORT,() =>{
-    console.log('server start');
-})
+app.listen(PORT, () => {
+  console.log("server start");
+});
 
-app.get('*', function (req,res) {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+// app.get('*', function (req,res) {
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+//   });
+
+app.post("/api/path", (req, res) => {
+    const start = req.body.start.place_name.replace(' ','%20');
+    const end = req.body.end.place_name.replace(' ','%20');
+    
+  getHtml(start,end).then((html) => {
+    const $ = cheerio.load(html.data);
+    
+    let parentTag = $("span").text();
+    console.log(parentTag)
+  })
+  res.json("success");
+});
+
+const getHtml = async (start, end) => {
+  try {
+    
+    const url = `https://map.kakao.com/?sName=${start}&eName=${end}`;
+    console.log(url);
+    return await axios.get(url);
+    // 해당 사이트 html 태그 가져오기
+  } catch (error) {
+    console.error(error);
+  }
+};
